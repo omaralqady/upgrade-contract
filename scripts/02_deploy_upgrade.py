8b5745e446936ff3165a19532ea39fff3e25856e
Omar Alqady <omar@founda.com>
2021-10-24 23:39:42 +0200

    feat: publish source to verify contracts

--- scripts/02_deploy_upgrade.py
+++ scripts/02_deploy_upgrade.py
@@ -12,9 +12,9 @@ from brownie import (
 def main():
     account = get_account()
     print(f"Deploying to : {network.show_active()}")
-    box = Box.deploy({"from": account})
+    box = Box.deploy({"from": account}, publish_source=True)
 
-    proxy_admin = ProxyAdmin.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy({"from": account}, publish_source=True)
 
     box_encoded_initializer_function = encode_function_data()
 
@@ -23,12 +23,13 @@ def main():
         proxy_admin.address,
         box_encoded_initializer_function,
         {"from": account, "gas_limit": 1000000},
+        publish_source=True,
     )
     print(f"Proxy deployed to {proxy}. You can now upgrade to V2!")
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
     proxy_box.store(1, {"from": account})
 
-    box_v2 = BoxV2.deploy({"from": account})
+    box_v2 = BoxV2.deploy({"from": account}, publish_source=True)
     upgrade_tx = upgrade(account, proxy, box_v2.address, proxy_admin=proxy_admin)
     print("Proxy has been upgraded!")
 
